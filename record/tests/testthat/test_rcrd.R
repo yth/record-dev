3db4eb65219bf43c2119ce73a70c43a2c656b5ea
Yuan Cao <yuan.cao.email@gmail.com>
2021-02-10 13:28:32 -0500

    Added test for r2c function

--- record/tests/testthat/test_rcrd.R
+++ record/tests/testthat/test_rcrd.R
@@ -2,7 +2,7 @@ test_that("test_rcrd_record_init_1", {
 	expect_false(record_init("temp") == 0)
 })
 
-# test_that("test_rcrd_r2c_1", {
-#	records = record_init("temp")
-#	expect_equal(r2c("string", records), "string")
-#})
+test_that("test_rcrd_r2c_1", {
+	records = record_init("temp")
+	expect_equal(r2c("string", records), "string")
+})
