fed6031764ecf873ff2221c7b42cead30e6eed63
Yuan Cao <yuan.cao.email@gmail.com>
2021-02-12 09:54:59 -0500

    BUGGY: For debug on another machine

--- record/src/rcrd.c
+++ record/src/rcrd.c
@@ -7,10 +7,20 @@
 #include <sys/mman.h> // mmap related facilities
 #include <stdint.h> // uint64_t
 #include <assert.h> // assert
+
 #include <sys/types.h> // open
 #include <sys/stat.h> // open
 #include <fcntl.h> // open
-#include <unistd.h>
+#include <unistd.h> // close
+#include <assert.h> // assert
+
+#include <stdio.h> // printf
+
+
+
+// Global Variable
+char* STORAGE;
+int fd;
 
 
 /**
@@ -21,25 +31,36 @@
  */
 SEXP record_init(SEXP path_name) {
 	// TODO: Raw file for now, in the future should be handled by an object.
-	const char* path = CHAR(STRING_ELT(path_name, 0));
-	int fd = open(path, O_RDWR);
-	if (fd < 0) {
-		fd = open("./tmp.txt", O_CREAT | O_RDWR, S_IRWXU);
-		assert(fd > -1);
-	}
+	// const char* path = CHAR(STRING_ELT(path_name, 0));
+	// fd = open(path, O_RDWR);
+	// if (fd < 0) {
+	// 	fd = open("./tmp.txt", O_CREAT | O_RDWR, S_IRWXU); // No O_TMPFILE
+	// 	assert(fd > -1);
+	// }
 
-	char* memory_file = (char*) mmap(NULL, // Don't care where
-									 1024 * 1024, // 1 MB
-									 PROT_READ | PROT_WRITE, // Read/writable
-									 MAP_SHARED, // Make changes visible
-									 fd, // the file descriptor we just created
-									 0); // Start from the start of the file
+	// No physical page error
+	// STORAGE = (char*) mmap(NULL, // Don't care where
+	// 						1024 * 1024, // 1 MB
+	// 						PROT_READ | PROT_WRITE, // Read/writable
+	// 						MAP_SHARED, // Make changes visible
+	// 						fd, // the file descriptor we just created
+	// 						0); // Start from the start of the file
+
+	// Simplest test
+	STORAGE = (char*) malloc(1024 * 1024);
+	printf("Storage: %p\n", STORAGE);
+	//                   12345678911234
+	const char* cster = "random string";
+	memcpy(STORAGE, cster, 14);
+	STORAGE[14] = '\0';
 
 	// uint64_t memory_ptr = reinterpret_cast<uint64_t>(memory_file);
-	uint64_t memory_ptr = (uint64_t) memory_file;
-	SEXP r_res = PROTECT(allocVector(INTSXP, 1));
-	SET_INTEGER_ELT(r_res, 0, memory_ptr);
+	uint64_t memory_ptr = (uint64_t) STORAGE;
+	SEXP r_res = PROTECT(allocVector(REALSXP, 1));
+	SET_REAL_ELT(r_res, 0, memory_ptr);
 	UNPROTECT(1);
+
+	// return Rf_ScalarInteger(memory_ptr); // Try this later
 	return r_res;
 }
 
@@ -51,10 +72,70 @@ SEXP record_init(SEXP path_name) {
  * @param  storage  SEXP that represents the storage
  * @return          SEXP that can act as a hash for the value in the store
  */
-SEXP r2c(SEXP r_object, SEXP storage) {
-	const char* res = "wrong result";
+SEXP r2c(SEXP r_string_object, SEXP storage) {
+	// TODO: Generalize what kind of R objects can be passed in
+
+	// TODO: Find a way to not clobbering other values
+	//		 Solution is probably creating an object that manages storage
+
+		// uint64_t memory_ptr = (uint64_t) REAL0(storage);
+	// char* memory_file = STORAGE; // (char*) memory_ptr;
+
+	// int i = 0;
+	// while (c_string[i] != '\0') {
+	// 	memory_file[i] = c_string[i];
+	// 	++i;
+	// }
+	// assert(i > 0);
+	// free(STORAGE);
+
+	// int fd2 = open("tmp.txt", O_CREAT | O_APPEND | O_RDWR, (mode_t) 0777);
+	// if (fd2 < 0) {
+	// 	perror("Could not open/create a file");
+	// }
+
+	const char* c_string = CHAR(STRING_ELT(r_string_object, 0));
+	printf("c_string: %s\n", c_string);
+	FILE* f = fopen("tmp.txt", "rw");
+	if (f == NULL) {
+		perror("Could not open file");
+	}
+	fprintf(f, "%s", c_string);
+	fclose(f);
+
+	// printf("printf: %s\n", c_string);
+	// for (int j = 0; c_string[j] != '\0'; ++j) {
+	// 	write(fd2, c_string + j, 1);
+	// 	write(0, c_string + j, 1);
+	// 	write(1, c_string + j, 1);
+	// 	write(2, c_string + j, 1);
+	// }
+
+	// Write test
+	// const char* cster = "random string";
+	// printf("\nprintf: %s\n", STORAGE);
+	// for (int j = 0; STORAGE + j != '\0'; ++j) {
+	// 	write(fd2, STORAGE + j, 1);
+	// 	write(0, STORAGE + j, 1);
+	// 	write(1, STORAGE + j, 1);
+	// 	write(2, STORAGE + j, 1);
+	// }
+
+	// if (close(fd2) < 0) {
+	// 	perror("Could not close file descriptor");
+	// }
+
+	// // Don't unmapp in future
+	// if (msync(memory_file, 100, MS_SYNC) < 0) {
+	// 	perror("Could not write to disk");
+	// } // Try MS_ASYNC in future
+	//
+	// if (munmap(memory_file, 1024 * 1024) < 0) {
+	// 	perror("Could not unmap");
+	// }
+
 	SEXP r_res = PROTECT(allocVector(STRSXP, 1));
-	SET_STRING_ELT(r_res, 0, mkChar(res));
+	SET_STRING_ELT(r_res, 0, mkChar(c_string));
 	UNPROTECT(1);
 
 	return r_res;
