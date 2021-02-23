af5e36ada88e43b8d43ecf237356e5416c2d5881
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-02-23 10:42:37 +0100

    removed record function

--- record/R/record.R
+++ record/R/record.R
@@ -1,6 +1,4 @@
 ## usethis namespace: start
 #' @useDynLib record, .registration = TRUE, .fixes = "RCRD_"
 ## usethis namespace: end
-record <- function(rvalue) {
-  .Call(C_record, rvalue)
-}
+NULL
