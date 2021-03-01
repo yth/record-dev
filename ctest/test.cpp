7a034147299ec10c908c39e83ad5f0a49ee3ca04
Yuan Cao <yuan.cao.email@gmail.com>
2021-03-01 14:50:08 -0500

    Adding minimal structure for testing c(pp) code

---
+++ ctest/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "helper.cpp"
+
+int main() {
+	std::cout << "Hello, World\n";
+	return 0;
+}
---
+++ record/src/helper.cpp
@@ -0,0 +1,20 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// Writes a size_t to file
+void write_size_t(FILE* file, size_t val) {
+	return;
+}
+
+
+// Read a size_t from file starting at the indicated offset
+size_t read_size_t(FILE* file, size_t file_offset, size_t value_offset) {
+	return 1;
+}
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
