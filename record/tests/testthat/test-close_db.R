8284c013739d80cc9f4b3ca3ea4064b3eea3318e
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-02 19:15:34 +0100

    add new close_db test

--- record/tests/testthat/test-close_db.R
+++ record/tests/testthat/test-close_db.R
@@ -1,4 +1,11 @@
 test_that("test_close_bd_1", {
-	file = open_db_for_write()
-	expect_equal(close_db(file), NULL)
+	open_db_for_write()
+	expect_equal(close_db(), NULL)
+})
+
+test_that("close after adding 100 elements", {
+  open_db_for_write("test.txt")
+  add_value(1:100)
+  has_value(1:100)
+  expect_equal(close_db(), NULL)
 })
