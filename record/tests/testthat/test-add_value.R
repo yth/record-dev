4ed8425762eafa92ebbccb7d16e0c7fba6ea7796
Yuan Cao <yuan.cao.email@gmail.com>
2021-03-02 14:55:19 -0500

    Add more test for adding values to database

--- record/tests/testthat/test-add_value.R
+++ record/tests/testthat/test-add_value.R
@@ -1,4 +1,4 @@
-test_that("test_rcrd_add_value_1", {
+test_that("test_rcrd_add_value_simple", {
 	success = open_db_for_write("tmp.txt")
 	expect_equal(success, NULL)
 	s = "hello"
@@ -8,4 +8,16 @@ test_that("test_rcrd_add_value_1", {
 	r = readBin(f, n = 1, character())
 	expect_gt(length(r), 0)
 	close(f)
+})
+
+test_that("test_rcrd_add_value_vector", {
+	open_db_for_write("test-close.txt")
+	add_value(1:10)
+	expect_equal(close_db(), NULL)
+})
+
+test_that("test_rcrd_add_value_larger", {
+	open_db_for_write("test-close2.txt")
+	add_value(1:100)
+	expect_equal(close_db(), NULL)
 })
