283f8e37bbeb51eb7c9f5d894307b8c3e25d3647
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-03 13:04:44 +0100

    test_has_value tests has_value only

--- record/tests/testthat/test_has_value.R
+++ record/tests/testthat/test_has_value.R
@@ -1,43 +1,38 @@
-if (F) {
 test_that("test_rcrd_has_value_1", {
-	success = open_db_for_write("tmp.txt")
-	expect_equal(success, NULL)
+	open_db_for_write("hello.txt")
 	s = "hello"
-	expect_equal(add_value(s), s)
+	add_value(s)
 	expect_equal(has_value(s), T)
-	expect_equal(close_db(), NULL)
+	close_db()
 })
 
 test_that("test_rcrd_has_value_2", {
-	success = open_db_for_write("tmp.txt")
-	expect_equal(success, NULL)
+  open_db_for_write("hello-goodbye.txt")
 	s1 = "hello"
 	s2 = "good bye"
-	expect_equal(add_value(s1), s1)
-	expect_equal(add_value(s2), s2)
+	add_value(s1)
+	add_value(s2)
 	expect_equal(has_value(s1), T)
 	expect_equal(has_value(s2), T)
-	expect_equal(close_db(), NULL)
+	close_db()
 })
 
 test_that("test_rcrd_has_value_3", {
-	success = open_db_for_write("tmp.txt")
-	expect_equal(success, NULL)
+	open_db_for_write("three-string-vals.txt")
 	s1 = "hello"
 	s2 = "good bye"
 	s3 = "arrivederci"
-	expect_equal(add_value(s1), s1)
-	expect_equal(add_value(s2), s2)
+	add_value(s1)
+	add_value(s2)
 	expect_equal(has_value(s1), T)
 	expect_equal(has_value(s2), T)
 	expect_equal(has_value(s3), F)
-	expect_equal(close_db(), NULL)
+	close_db()
 })
 
 test_that("test with a vector of length 100", {
-  open_db_for_write("test-100.txt")
+  open_db_for_write("has-100-value.txt")
   add_value(1:100)
   expect_equal(has_value(1:100), TRUE)
-  expect_equal(close_db(), NULL)
+  close_db()
 })
-}
