188721993a6f99d835a6b3b17d146bf62e03dedb
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-03 13:00:53 +0100

    test_add_value tests add_value only

--- record/tests/testthat/test_add_value.R
+++ record/tests/testthat/test_add_value.R
@@ -11,13 +11,20 @@ test_that("test_rcrd_add_value_simple", {
 })
 
 test_that("test_rcrd_add_value_vector", {
-	open_db_for_write("test-close.txt")
-	add_value(1:10)
-	expect_equal(close_db(), NULL)
+	open_db_for_write("add10.txt")
+	expect_equal(length(add_value(1:10)), 10)
+	close_db()
 })
 
 test_that("test_rcrd_add_value_larger", {
-	open_db_for_write("test-close2.txt")
-	add_value(1:100)
-	expect_equal(close_db(), NULL)
+	open_db_for_write("add100.txt")
+	expect_equal(length(add_value(1:100)), 100)
+	close_db()
+})
+
+test_that("add-alphabet", {
+	open_db_for_write("add-alphabet.txt")
+  alphabet <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+	expect_equal(length(add_value(alphabet)), 26)
+	close_db()
 })
