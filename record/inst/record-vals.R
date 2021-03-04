c648a51ea739fc4f63ee784ed74794bc38a0707b
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-05 00:12:31 +0100

    update script

---
+++ record/inst/record-vals.R
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+
+library(signatr)
+library(record)
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) < 1) {
+  message("Usage: merge-gbov.R <dir> [... <fileN>]")
+  q(status=1)
+}
+
+run_dir <- args[1]
+package_name <- args[2]
+
+
+if (!dir.exists(run_dir)) {
+  stop(run_dir, ": no such a directory")
+}
+
+val_files <- list.files(path = run_dir, pattern = "values.RDS", recursive = TRUE)
+
+tictoc::tic("Recording started:")
+cat(sprintf("Recording started ...\n\n"))
+
+if(is.null(open_db_for_write(paste0(package_name, ".txt")))) {
+  print(paste0(package_name, ".txt", " opened successfully."))
+}
+
+num_total_val <- 0
+
+for (i in seq_along(val_files)) {
+  val_list <- readRDS(paste0(run_dir, "/", val_files[[i]]))
+  vals <- lapply(val_list, function(x) x[[3]])
+  num_total_val <- num_total_val + length(vals)
+
+  lapply(vals, add_val)
+  ## lapply(vals, has_seen)
+
+  ## for(val in vals) {
+  ##   add_val(val)
+  ##   if(typeof(val) == "closure" || typeof(val) == "environment" || typeof(val) == "builtin" || typeof(val) == "special") {
+  ##     print(paste0("special typed value recorded."))
+  ##   } else {
+  ##     print(paste0(toString(val), " recorded."))
+  ##     if(has_seen(val)) {
+  ##       print(paste0(toString(val), " found."))
+  ##     } else {
+  ##       print(paste0(toString(val), " not found."))
+  ##     }
+  ##   }
+  ## }
+}
+
+print(paste0("there were a total of  ", toString(num_total_val), " values."))
+print(paste0("counted  ", toString(count_val()), " many values."))
+
+stopifnot(num_total_val == count_val())
+
+if(is.null(close_db())) {
+  print(paste0(package_name, ".txt", " closed successfully."))
+}
+
+cat(sprintf("Recording %s values done.\n\n", num_total_val))
+tictoc::toc()
