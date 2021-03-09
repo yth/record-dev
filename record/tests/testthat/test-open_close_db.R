886cb1be7eb198f5ccf0bf4f05755044dd1200ae
Yuan Cao <yuan.cao.email@gmail.com>
2021-03-08 22:18:44 -0500

    Add test to test for open_db_for_read

--- record/tests/testthat/test-open_close_db.R
+++ record/tests/testthat/test-open_close_db.R
@@ -1,4 +1,8 @@
 test_that("test_open_close_bd_1", {
-	expect_equal(open_db_for_write(), NULL)
+	expect_equal(open_db_for_write("test_dir"), NULL)
 	expect_equal(close_db(), NULL)
+})
+
+test_that("test_open_db_for_read_1", {
+	expect_equal(open_db_for_read("test_dir"), NULL)
 })
