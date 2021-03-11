71a342d0bd874d3a1595e3852568f4ae965acacb
Yuan Cao <yuan.cao.email@gmail.com>
2021-03-10 21:06:21 -0500

    Add test for new open_db interface

--- record/tests/testthat/test-open_close_db.R
+++ record/tests/testthat/test-open_close_db.R
@@ -3,10 +3,17 @@ test_that("test_open_close_bd_1", {
 	expect_equal(close_db(), NULL)
 })
 
-test_that("test_open_db_for_read_1", {
-	expect_equal(open_db_for_read("test_db"), NULL)
+test_that("test_open_db_1", {
+	expect_silent(open_db("test_db__dir_1", create = T))
 })
 
-test_that("test_open_db_for_read_2", {
-	expect_error(open_db_for_read("test_db_2"), NULL)
+test_that("test_open_db_2", {
+	# expect_silent(open_db("test_db_dir_2", create = T))
+	# expect_silent(close_db())
 })
+
+test_that("test_open_db_3", {
+	# expect_silent(open_db("test_db_dir_2", create = F))
+	# expect_silent(close_db())
+})
+
