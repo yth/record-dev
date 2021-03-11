6f822b665f447651f157cea48adc598763d3c12b
Yuan Cao <yuan.cao.email@gmail.com>
2021-03-10 22:19:08 -0500

    Merged tests

--- record/tests/testthat/test-open_close_db.R
+++ record/tests/testthat/test-open_close_db.R
@@ -5,14 +5,11 @@ test_that("test_open_close_bd_1", {
 
 test_that("test_open_db_1", {
 	expect_silent(open_db("test_db__dir_1", create = T))
+	expect_silent(close_db())
+	setwd("..")
 })
 
 test_that("test_open_db_2", {
-	expect_silent(open_db("test_db_dir_2", create = T))
-	expect_silent(close_db())
-})
-
-test_that("test_open_db_3", {
 	# expect_silent(open_db("test_db_dir_2", create = F))
 	# expect_silent(close_db())
 })
