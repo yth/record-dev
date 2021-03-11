99cbcb2457e7ed7c9176f93f25fa52da8283c42c
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-11 20:22:50 +0100

    tests for the new version of open_db

--- record/tests/testthat/test-1_open_close_db.R
+++ record/tests/testthat/test-1_open_close_db.R
@@ -1,16 +1,27 @@
-test_that("test_open_close_bd_1", {
-	expect_equal(open_db_for_write("test_db"), NULL)
+test_that("open and close", {
+	open_db_for_write("open-test.txt")
+  expect_equal(file.exists("open-test.txt"), TRUE)
 	expect_equal(close_db(), NULL)
 })
 
-test_that("test_open_db_1", {
-	expect_silent(open_db("test_db__dir_1", create = T))
+test_that("open a new db and close it", {
+	open_db("test_db", create = TRUE)
+  expect_equal(dir.exists("test_db"), TRUE)
 	expect_silent(close_db())
-	setwd("..")
 })
 
-test_that("test_open_db_2", {
-	expect_error(open_db("test_db_dir_2", create = F))
-	setwd("..")
+test_that("open existing db for read", {
+	open_db("test_db", create = FALSE)
+  expect_equal(file.exists("test_db/gbov.bin"), TRUE)
+  expect_equal(file.exists("test_db/indices.bin"), TRUE)
+	close_db()
 })
 
+test_that("open existing db", {
+	expect_error(open_db("test_db", create = TRUE))
+  ## close_db() =>  segfault
+})
+
+test_that("test open non existing db for read", {
+	expect_error(open_db("non_existing_db", read_only = TRUE))
+})
