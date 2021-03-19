cc3f2d9961a636939fbd4cc2419acb26a3bb20bd
Yuan Cao <yuan.cao.email@gmail.com>
2021-03-19 06:00:50 -0400

    Strengthen test

--- record/tests/testthat/test-6_get_random_val.R
+++ record/tests/testthat/test-6_get_random_val.R
@@ -6,12 +6,35 @@ test_that("get random val hello after adding hello", {
 	close_db()
 })
 
+test_that("get random val hello after adding hello redux", {
+	open_db("test_db/get_random_1")
+	s1 = "hello"
+	expect_equal(get_random_val(), s1)
+	close_db()
+})
+
 test_that("get all three added vals by get_random_val in a loop", {
 	open_db("test_db/get_random_2", create = TRUE)
 	add_val(TRUE)
 	add_val(1)
 	add_val("hello")
 
+  vals_added <- list(TRUE, 1, "hello")
+
+  rand_vals = vector('list', 100)
+	for (i in seq_along(rand_vals)) {
+		rand_vals[[i]] <- get_random_val()
+	}
+
+  for(rand_val in rand_vals) {
+    expect_true(list(rand_val) %in% vals_added)
+  }
+
+	close_db()
+})
+
+test_that("get all three added vals by get_random_val in a loop redux", {
+	open_db("test_db/get_random_2")
   vals_added <- list(TRUE, 1, "hello")
 
   rand_vals = vector('list', 100)
@@ -43,3 +66,4 @@ test_that("get random val from stringr::str_detect from existing db", {
 
 	close_db()
 })
+
