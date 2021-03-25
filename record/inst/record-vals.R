0e68bd1508564f7a4314d8e332a5f3ad141f707f
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-25 14:59:33 +0100

    update script to test int_db

--- record/inst/record-vals.R
+++ record/inst/record-vals.R
@@ -19,36 +19,36 @@ if (!dir.exists(run_dir)) {
 
 val_files <- list.files(path = run_dir, pattern = "values.RDS", recursive = TRUE)
 
-## if(is.null(open_db(paste0(package_name, "_db.txt"), create = TRUE))) {
-##   print(paste0(package_name, ".txt", " opened successfully."))
-## }
+if(is.null(open_db(paste0(package_name, "_db"), create = TRUE))) {
+  print(paste0(package_name, "_db", " opened successfully."))
+}
 
 tictoc::tic("Recording started:")
 cat(sprintf("Recording started ...\n\n"))
 
 gbov <- list()
-## gbov <- data.frame(byte = numeric(), type = character(), val = list())
 
 for (i in seq_along(val_files)) {
   val_list <- readRDS(paste0(run_dir, "/", val_files[[i]]))
   vals <- lapply(val_list, function(x) x[[3]])
 
-  ## lapply(vals, add_val)
-  gbov <- append(gbov, vals)
+  lapply(vals, add_val)
+  ## gbov <- append(gbov, vals)
 }
 
-duplicates <- sum(duplicated(gbov))
-u_gbov <- unique(gbov)
-## print(pryr::object_size(gbov))
+## duplicates <- sum(duplicated(gbov))
+## u_gbov <- unique(gbov)
 
-print(paste0(duplicates, " duplicates are found out of ", length(gbov), " values"))
-print(paste0(length(gbov) - length(u_gbov), " should equal to ", duplicates))
-## print(paste0(count_vals(), " - ", size_db(), " = ", count_vals() - size_db()))
+## print(paste0(duplicates, " duplicates are found out of ", length(gbov), " values"))
+## print(paste0(length(gbov) - length(u_gbov), " should equal to ", duplicates))
+print(paste0("total of ", count_vals(), " saw."))
+print(paste0("size of db (= num of unique values) is ", size_db()))
+print(paste0("size of int_db (= num of unique int values) is ", size_ints()))
 
-saveRDS(u_gbov, file = "unique_vals.RDS")
-## if(is.null(close_db())) {
-##   print(paste0(package_name, ".txt", " closed successfully."))
-## }
+## saveRDS(u_gbov, file = "unique_vals.RDS")
+if(is.null(close_db())) {
+  print(paste0(package_name, "_db", " closed successfully."))
+}
 
 cat(sprintf("Recording done.\n\n"))
 tictoc::toc()
