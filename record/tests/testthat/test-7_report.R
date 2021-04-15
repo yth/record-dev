6197fa1900db74fbe6b822f067e2be47a4030910
Yuan Cao <yuan.cao.email@gmail.com>
2021-04-15 12:20:41 -0400

    Add simple test for report

---
+++ record/tests/testthat/test-7_report.R
@@ -0,0 +1,8 @@
+test_that("print some report", {
+	open_db("test_db/some_report", create = TRUE)
+	s1 = "hello"
+	add_val(s1)
+	expect_equal(get_random_val(), s1)
+	expect_silent(report())
+	close_db()
+})
