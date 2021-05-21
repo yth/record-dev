2960e077686043c2343b716da1ce73537dbf7fcd
Yuan Cao <yuan.cao.email@gmail.com>
2021-05-20 23:08:42 -0400

    Adjust test to add a raw value

--- record/tests/testthat/test-7_report.R
+++ record/tests/testthat/test-7_report.R
@@ -5,6 +5,7 @@ test_that("print some report", {
 	open_db("test_db/some_report", create = TRUE)
 	s1 = "hello"
 	add_val(s1)
+	add_val(as.raw(1))
 	add_val(1)
 	add_val(1L)
 	expect_silent(report())
