50c28e31326dbae62b43f970f77ddc7a10f2e82b
Yuan Cao <yuan.cao.email@gmail.com>
2021-05-26 20:22:42 -0400

    Reformat tests

--- record/tests/testthat/test-5_have_seen.R
+++ record/tests/testthat/test-5_have_seen.R
@@ -36,24 +36,26 @@ test_that("open existing db and not seen not existing val", {
 })
 
 test_that("have seen 100 times", {
-  open_db("test_db/seen-100", create = TRUE)
-  for(i in 1:100){
-    add_val(i)
-  }
-  add_val("hello")
-  for(i in 1:100){
-    expect_equal(have_seen("hello"), TRUE)
-    expect_equal(have_seen(i), TRUE)
-  }
-  close_db()
+	open_db("test_db/seen-100", create = TRUE)
+
+	for(i in 1:100) {
+		add_val(i)
+	}
+	add_val("hello")
+
+	for(i in 1:100) {
+		expect_equal(have_seen("hello"), TRUE)
+		expect_equal(have_seen(i), TRUE)
+	}
+	close_db()
 })
 
 test_that("have seen alphabet", {
-  open_db("test_db/seen-alphabet", create = TRUE)
-  alphabet <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-  add_val(alphabet)
-  expect_equal(have_seen(alphabet), TRUE)
-  close_db()
+	open_db("test_db/seen-alphabet", create = TRUE)
+	alphabet <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+	add_val(alphabet)
+	expect_equal(have_seen(alphabet), TRUE)
+	close_db()
 })
 
 # real values from stringr::str_detect
@@ -61,22 +63,20 @@ val_list <- readRDS("../resource/values.RDS")
 vals <- lapply(val_list, function(x) x[[3]])
 
 test_that("have seen real vals from stringr::str_detect", {
+	open_db("test_db/str_detect", create = TRUE)
 
+	lapply(vals, add_val)
 
-  open_db("test_db/str_detect", create = TRUE)
+	for(val in vals) {
+		expect_equal(have_seen(val), TRUE)
+	}
 
-  lapply(vals, add_val)
-
-  for(val in vals) {
-    expect_equal(have_seen(val), TRUE)
-  }
-
-  close_db()
+	close_db()
 })
 
 test_that("remove all information after closing database", {
-  u_vals <- unique(vals)
-  expect_false(size_db() == length(u_vals))
+	u_vals <- unique(vals)
+	expect_false(size_db() == length(u_vals))
 })
 
 }
--- record/tests/testthat/test-7_report.R
+++ record/tests/testthat/test-7_report.R
@@ -5,10 +5,15 @@ test_that("print some report", {
 	open_db("test_db/some_report", create = TRUE)
 	s1 = "hello"
 	add_val(s1)
+	add_val("a really long string should be in generic store")
 	add_val(as.raw(1))
 	add_val(1)
 	add_val(1L)
 	expect_silent(report())
+	close_db()
+
+	open_db("test_db/some_report")
+	report()
 	close_db()
 })
 
