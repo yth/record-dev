372e01d1fa645f19b523b5c7c5183c7e4276bf84
Yuan Cao <yuan.cao.email@gmail.com>
2021-05-26 20:37:18 -0400

    Hide functions that doesn't need to be exposed to R

--- record/src/init.c
+++ record/src/init.c
@@ -41,28 +41,16 @@ static const R_CallMethodDef callMethods[] = {
 	{"init_simple_int_store",	(DL_FUNC) &init_simple_int_store,	1},
 	{"load_simple_int_store",	(DL_FUNC) &load_simple_int_store,	1},
 	{"close_simple_int_store",	(DL_FUNC) &close_simple_int_store,	0},
-	{"is_simple_int",			(DL_FUNC) &is_simple_int,			1},
-	{"add_simple_int",			(DL_FUNC) &add_simple_int,			1},
-	{"have_seen_simple_int",	(DL_FUNC) &have_seen_simple_int,	1},
-	{"get_simple_int",			(DL_FUNC) &get_simple_int,			1},
 
 	// simple doubles store related
 	{"init_simple_dbl_store",	(DL_FUNC) &init_simple_dbl_store,	1},
 	{"load_simple_dbl_store",	(DL_FUNC) &load_simple_dbl_store,	1},
 	{"close_simple_dbl_store",	(DL_FUNC) &close_simple_dbl_store,	0},
-	{"is_simple_dbl",			(DL_FUNC) &is_simple_dbl,			1},
-	{"add_simple_dbl",			(DL_FUNC) &add_simple_dbl,			1},
-	{"have_seen_simple_dbl",	(DL_FUNC) &have_seen_simple_dbl,	1},
-	{"get_simple_dbl",			(DL_FUNC) &get_simple_dbl,			1},
 
 	// simple raw store related
 	{"init_simple_raw_store",	(DL_FUNC) &init_simple_raw_store,	1},
 	{"load_simple_raw_store",	(DL_FUNC) &load_simple_raw_store,	1},
 	{"close_simple_raw_store",	(DL_FUNC) &close_simple_raw_store,	0},
-	{"is_simple_raw",			(DL_FUNC) &is_simple_raw,			1},
-	{"add_simple_raw",			(DL_FUNC) &add_simple_raw,			1},
-	{"have_seen_simple_raw",	(DL_FUNC) &have_seen_simple_raw,	1},
-	{"get_simple_raw",			(DL_FUNC) &get_simple_raw,			1},
 
 	// simple string store related
 	{"init_simple_str_index",	(DL_FUNC) &init_simple_str_index,	1},
@@ -71,10 +59,6 @@ static const R_CallMethodDef callMethods[] = {
 	{"init_simple_str_store",	(DL_FUNC) &init_simple_str_store,	1},
 	{"load_simple_str_store",	(DL_FUNC) &load_simple_str_store,	1},
 	{"close_simple_str_store",	(DL_FUNC) &close_simple_str_store,	0},
-	{"is_simple_str",			(DL_FUNC) &is_simple_str,			1},
-	{"add_simple_str",			(DL_FUNC) &add_simple_str,			1},
-	{"have_seen_simple_str",	(DL_FUNC) &have_seen_simple_str,	1},
-	{"get_simple_str",			(DL_FUNC) &get_simple_str,			1},
 
 	// generic R value store related
 	{"init_generic_index",		(DL_FUNC) &init_generic_index,		1},
