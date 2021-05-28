56a3dc30829d00807bac26c047fb363b5519631d
Yuan Cao <yuan.cao.email@gmail.com>
2021-05-28 03:08:58 -0400

    Make test give more information

--- record/tests/testthat/test-7_report.R
+++ record/tests/testthat/test-7_report.R
@@ -1,6 +1,7 @@
 if (T) {
 
 test_that("print some report", {
+	print("Call report() before opening a new database")
 	report()
 
 	open_db("test_db/some_report", create = TRUE)
@@ -10,10 +11,12 @@ test_that("print some report", {
 	add_val(as.raw(1))
 	add_val(1)
 	add_val(1L)
+	print("Call report() after opening a new database and after doing work")
 	expect_silent(report())
 	close_db()
 
 	open_db("test_db/some_report")
+	print("Call report() after reopening the database")
 	report()
 	close_db()
 })
